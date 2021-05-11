892e74be56b5be51217f3d4352022dc7cce95160
ygmllt <ygmllt@gmail.com>
2021-05-11 08:30:50 +0300

    Third tasks: Objects and arrays

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,11 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
+  if (!obj) return;
 
+  return Object.keys(obj).reduce((acc, key) => {
+    acc[obj[key]] = key;
+
+    return acc;
+  }, {});
 }
--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,27 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (string && !size && size !== 0) {
+    return string;
+  }
 
+  if (size === 0) {
+    return '';
+  }
+
+  let count = 0;
+  let lastChar = null;
+  const split = string.split('');
+  const result = [];
+
+  split.forEach(char => {
+    count = char !== lastChar ? 1 : ++count;
+    lastChar = char;
+
+    if (count <= size) {
+      result.push(char);
+    }
+  });
+
+  return result.join('');
 }
